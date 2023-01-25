323aa44a4426e76239624ec7d718234bbe744fae
ColbyCabrera <gdthyispro@gmail.com>
2023-01-25 10:05:38 -0500

    Add navigation menu items

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,21 +1,43 @@
 export default pageLoad;
-import restaurantImg from './weenieHutJr.webp';
+import restaurantImg from "./weenieHutJr.webp";
 
 function pageLoad() {
-    const body = document.getElementsByTagName('body')[0];
-    const content = document.createElement('div');
-    const header = document.createElement('h1');
-    const restaurantImage = new Image();
-    const restaurantDescription = document.createElement('p');
+  const body = document.getElementsByTagName("body")[0];
+  const header = document.createElement("header");
+  const content = document.createElement("div");
+  const restaurantName = document.createElement("h1");
+  const restaurantImage = new Image();
+  const restaurantDescription = document.createElement("p");
 
-    document.title = "Weenie Hut Jr's";
-    content.id = 'content';
-    header.textContent = "Weenie Hut Jr's";
-    restaurantImage.src = restaurantImg;
-    restaurantDescription.textContent = 'The best restaurant for weenies and nerds';
-    
-    body.appendChild(content);
-    content.appendChild(header);
-    content.appendChild(restaurantImage);
-    content.appendChild(restaurantDescription);
+  document.title = "Weenie Hut Jr's";
+  content.id = "content";
+  restaurantName.textContent = "Weenie Hut Jr's";
+  restaurantImage.src = restaurantImg;
+  restaurantDescription.textContent =
+    "The best restaurant for weenies and nerds";
+
+  body.appendChild(header);
+  body.appendChild(content);
+  content.appendChild(restaurantName);
+  content.appendChild(restaurantImage);
+  content.appendChild(restaurantDescription);
+
+  const nav = document.createElement("nav");
+  const navList = document.createElement("ul");
+  const homeTab = document.createElement("li");
+  const menuTab = document.createElement("li");
+  const contactTab = document.createElement("li");
+  homeTab.id = 'home';
+  menuTab.id = 'menu';
+  contactTab.id = 'contact';
+
+  homeTab.textContent = "Home";
+  menuTab.textContent = "Menu";
+  contactTab.textContent = "Contact";
+
+  header.appendChild(nav);
+  nav.appendChild(navList);
+  navList.appendChild(homeTab);
+  navList.appendChild(menuTab);
+  navList.appendChild(contactTab);
 }
